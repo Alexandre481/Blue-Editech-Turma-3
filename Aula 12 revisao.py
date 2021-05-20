17182b8aef72f7b80709752102c41c141803fb26
Alexandre <alexandrecgonzaga@gmail.com>
2021-05-19 21:29:05 -0300

    AULA 13

---
+++ Aula 12 revisao.py
@@ -0,0 +1,62 @@
+####REVISÃO FUNÇÃO
+##def somar():
+    ###num1=float(input("Entre o primeiro numero:"))
+   ## num2=float(input("Entre o segundo numero:"))
+    ###soma=num1+num2
+   ## print(soma)
+##somar()
+
+##def somar(numa,numb):
+   ## print("O primeiro numero é:",numa)
+    ##print("O primeiro numero é:",numb)
+    ##soma=numa+numb
+    ##print("A soma é:",soma)
+
+##num1=float(input("Entre o primeiro numero:"))
+##num2=float(input("Entre o segundo numero:"))
+##soma=num1+num2
+##print("resultado:",soma)
+
+
+
+
+
+###Exercício Treino - Crie um dicionário em que suas chaves serão os números 1, 4, 5, 6,
+##7, e 9 (que podem ser armazenados em uma lista) e seus valores correspondentes
+### aos quadrados desses números.###
+
+
+##numeros=[1,2,3,4,5,6,7,9]
+
+
+
+
+###resultado={a:a**2 for a in range(1,10)}
+
+###resultado2={a:a**2 for a in [1,4,5,6,7,9]}
+
+###print (resultado)
+##print (resultado2)
+
+##fusao=dobro.insert(numeros)
+
+
+
+
+###  Exercício Treino - Crie um dicionário em que suas chaves correspondem a números
+##   inteiros entre [1, 10] e cada valor associado é o número ao quadrado.
+##n
+
+## Faça um programa que leia nome e média de um aluno, guardando também a situação
+## em um dicionário. No final, mostre o conteúdo sa estrutura na tela. A média para
+## aprovação é 7. Se o aluno tirar entre 5 e 6.9 está de recuperação, caso contrário é
+## reprovado.
+##aluno_media={"Silvio":"8","Helena":"5","Santana":"7","Cleo":"4","Tamara":"3"}
+###print(0<7,"Aprovado:")
+
+
+
+
+
+##3lista=[1,4,5,6,7,9]
+###print{}
